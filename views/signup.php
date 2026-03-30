9a7df9898c04c7de3714ff3f5fed11f5675d9cba
chaaaos3 <schmolling.luise@gmail.com>
2026-03-30 14:59:20 +0200

    Add: Sign Up view

---
+++ views/signup.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign Up</title>
+</head>
+
+<body>
+  <h1>Sign Up</h1>
+  <form action="../index.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br><br>
+    <input type="submit" value="Sign Up">
+  </form>
+</body>
+
+</html>
