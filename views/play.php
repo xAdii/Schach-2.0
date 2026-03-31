16abd6c22a323ef83ba9d6e4b364bccfa05e93b7
xAdii <nekrum7@gmail.com>
2026-03-31 15:13:56 +0200

    Refactor: enhance play view layout with board structure

--- views/play.php
+++ views/play.php
@@ -1,1 +1,13 @@
 <h1>Play</h1>
+<div class="board">
+    <?php
+    for ($i = 0; $i < 8; $i++) {
+        echo '<div class="row">';
+        for ($j = 0; $j < 8; $j++) {
+            $color = ($i + $j) % 2 === 0 ? 'white' : 'black';
+            echo '<div class="field ' . $color . '"></div>';
+        }
+        echo '</div>';
+    }
+    ?>
+</div>
