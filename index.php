afe904e7fc5c9b33ccaf7c952a5014b94d5ce2d3
RON1N2002 <niklas.foerster02@gmail.com>
2026-03-31 12:58:44 +0200

    Add: include Lexend font and base.css

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $userController->handleRequest();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./styles/base.css">
 </head>
 
 <body>
