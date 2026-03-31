4926002150043a547422cd7e79613eb38b1eff7d
KawaiiZensi <alanszestowicki@gmail.com>
2026-03-31 11:08:04 +0200

    Add: PHP_SELF Remove: HTML

--- views/login.php
+++ views/login.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Anmeldung</title>
-</head>
-<body>
 	<h2>Anmelden</h2>
-	<form action="../controllers/userController.php" method="post">
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 		<label for="username">Benutzername</label><br>
 		<input type="text" id="username" name="username" required><br>
 		<label for="password">Passwort</label><br>
 		<input type="password" id="password" name="password" required><br>
 		<input type="submit" value="Anmelden">
 	</form>
-</body>
-</html>
