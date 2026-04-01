8ef40700211c035740e5c850550805b1df447628
xAdii <nekrum7@gmail.com>
2026-04-01 11:23:47 +0200

    Add: update board styling and initialize black pieces with new images

--- views/play.php
+++ views/play.php
@@ -3,8 +3,8 @@
 <div class="board">
     <?php
     $board = [
-        [],
-        [new Placeholder('black', [1, 0]), new Placeholder('black', [1, 1]), new Placeholder('black', [1, 2]), new Placeholder('black', [1, 3]), new Placeholder('black', [1, 4]), new Placeholder('black', [1, 5]), new Placeholder('black', [1, 6]), new Placeholder('black', [1, 7])],
+        [new Rook('black', [0, 0]), new Knight('black', [0, 1]), new Bishop('black', [0, 2]), new Queen('black', [0, 3]), new King('black', [0, 4]), new Bishop('black', [0, 5]), new Knight('black', [0, 6]), new Rook('black', [0, 7])],
+        [new Pawn('black', [1, 0]), new Pawn('black', [1, 1]), new Pawn('black', [1, 2]), new Pawn('black', [1, 3]), new Pawn('black', [1, 4]), new Pawn('black', [1, 5]), new Pawn('black', [1, 6]), new Pawn('black', [1, 7])],
         [],
         [],
         [],
