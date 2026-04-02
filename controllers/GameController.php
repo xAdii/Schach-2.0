fc2039ff7c63c0601475833aa74795ff47d34f43
xAdii <nekrum7@gmail.com>
2026-04-02 09:05:00 +0200

    Fix: retrieve board ID correctly and streamline form submissions in play view

--- controllers/GameController.php
+++ controllers/GameController.php
@@ -45,7 +45,7 @@ class GameController
 
         $this->gameModel->insertBoard($userID, $userID);
 
-        $boardID = $this->gameModel->fetchMaxBoardID();
+        $boardID = $this->gameModel->fetchMaxBoardID()['ID'];
 
         $this->gameModel->insertPiece($boardID, "rook", "white", 0, 0);
         $this->gameModel->insertPiece($boardID, "knight", "white", 0, 1);
@@ -79,10 +79,7 @@ class GameController
     public function handleJoinGame($boardID = null)
     {
         if ($boardID === null) {
-            if (!isset($_POST['boardID']) || empty($_POST['boardID'])) {
-                return;
-            }
-            $boardID = $_POST['boardID'];
+            $boardID = $_POST['boardID'] ?? null;
         }
 
         unset($_SESSION['game']);
--- views/play.php
+++ views/play.php
@@ -5,6 +5,7 @@ $boards = $gameController->getUserGames();
 
 <h1>Spiele</h1>
 <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <input type="hidden" name="nav" value="play">
     <input type="hidden" name="action" value="createGame">
     <button type="submit">Neues Spiel erstellen</button>
 </form>
@@ -16,6 +17,7 @@ $boards = $gameController->getUserGames();
             <p>Weiß: <?= $board['playerWhiteID'] ?></p>
             <p>Schwarz: <?= $board['playerBlackID'] ?></p>
             <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <input type="hidden" name="nav" value="play">
                 <input type="hidden" name="action" value="joinGame">
                 <input type="hidden" name="boardID" value="<?= $board['ID'] ?>">
                 <button type="submit">Beitreten</button>
