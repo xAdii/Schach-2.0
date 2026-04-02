741ebf61912fb3e579a26cd16673da92b95ad53f
KawaiiZensi <alanszestowicki@gmail.com>
2026-04-02 12:58:49 +0200

    Refactor: "Willkommen zurück," wurde zu "Willkommen," bei der ersten Registrierung geändert.

--- views/home.php
+++ views/home.php
@@ -3,6 +3,6 @@
 if (!isset($_SESSION['user'])) : ?>
     <p>Willkommen auf Schach 2.0! Bitte logge dich ein oder registriere dich, um zu spielen.</p>
 <?php else : ?>
-    <p>Willkommen zurück, <?= htmlspecialchars($_SESSION['user']['name']); ?>!</p>
+    <p>Willkommen, <?= htmlspecialchars($_SESSION['user']['name']); ?>!</p>
     <p>Du kannst jetzt ein neues Spiel erstellen oder einem bestehenden Spiel beitreten.</p>
 <?php endif; ?>
