ff41fb1fb01095d64a18749b016dcf69613826d1
RON1N2002 <niklas.foerster02@gmail.com>
2026-04-02 12:33:13 +0200

    Refactor: enhance button classes for improved styling and consistency

--- views/play.php
+++ views/play.php
@@ -6,7 +6,7 @@ $boards = $gameController->getUserGames();
 <h1>Spiele</h1>
 <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
     <input type="hidden" name="nav" value="play">
-    <button type="submit" name="action" value="createGame">Neues Spiel erstellen</button>
+    <button type="submit" name="action" value="createGame" class="create_new_game">Neues Spiel erstellen</button>
 </form>
 
 <div>
@@ -18,8 +18,8 @@ $boards = $gameController->getUserGames();
             <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                 <input type="hidden" name="nav" value="play">
                 <input type="hidden" name="boardID" value="<?= $board['ID'] ?>">
-                <button type="submit" name="action" value="joinGame">Beitreten</button>
-                <button type="submit" name="action" value="deleteGame">Löschen</button>
+                <button type="submit" name="action" value="joinGame" class="join_game">Beitreten</button>
+                <button type="submit" name="action" value="deleteGame" class="delete_game">Löschen</button>
             </form>
 
         </div>
