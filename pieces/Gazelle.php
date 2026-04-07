a0ddcacb351a83b2c71ae85cc8f4f57aeea61997
RON1N2002 <niklas.foerster02@gmail.com>
2026-04-07 13:23:18 +0200

    Fix: Gazelle movement

--- pieces/Gazelle.php
+++ pieces/Gazelle.php
@@ -4,10 +4,18 @@ class Gazelle extends GenericPiece
   public function __construct($color, $position_y, $position_x)
   {
     parent::__construct($color, $position_y, $position_x, [
+      [[-2, -1]],
       [[-2, 0]],
-      [[0, -2]],
+      [[-2, 1]],
+      [[-1, -2]],
+      [[-0, -2]],
+      [[1, -2]],
+      [[-1, 2]],
+      [[0, 2]],
+      [[1, 2]],
+      [[2, 1]],
       [[2, 0]],
-      [[0, 2]]
+      [[2, -1]]
     ], "./images/" . $color . "/gazelle.png");
   }
 }
