88d8928942b02bd76f9ba39ae8d699f187112655
chaaaos3 <schmolling.luise@gmail.com>
2026-04-07 14:10:54 +0200

    FIX: deault boards

--- schach_20.sql
+++ schach_20.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 07. Apr 2026 um 13:32
+-- Erstellungszeit: 07. Apr 2026 um 14:10
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -33,8 +33,15 @@ CREATE TABLE `boards` (
   `playerBlackID` int(11) NOT NULL,
   `state` varchar(30) NOT NULL DEFAULT 'alive',
   `turn` varchar(30) NOT NULL DEFAULT 'white',
-  `shopID` int(8) NOT NULL
+  `shopID` int(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `boards`
+--
+
+INSERT INTO `boards` (`ID`, `playerWhiteID`, `playerBlackID`, `state`, `turn`, `shopID`) VALUES
+(20, 6, 6, 'alive', 'white', NULL);
 
 -- --------------------------------------------------------
 
@@ -65,6 +72,45 @@ CREATE TABLE `pieces` (
   `position_x` int(1) NOT NULL,
   `has_moved` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `pieces`
+--
+
+INSERT INTO `pieces` (`ID`, `boardID`, `type`, `color`, `position_y`, `position_x`, `has_moved`) VALUES
+(386, 20, 'rook', 'white', 7, 0, 0),
+(387, 20, 'knight', 'white', 7, 1, 0),
+(388, 20, 'bishop', 'white', 7, 2, 0),
+(389, 20, 'queen', 'white', 7, 3, 0),
+(390, 20, 'king', 'white', 7, 4, 0),
+(391, 20, 'bishop', 'white', 7, 5, 0),
+(392, 20, 'knight', 'white', 7, 6, 0),
+(393, 20, 'rook', 'white', 7, 7, 0),
+(394, 20, 'pawn', 'white', 6, 0, 0),
+(395, 20, 'pawn', 'white', 6, 1, 0),
+(396, 20, 'pawn', 'white', 6, 2, 0),
+(397, 20, 'pawn', 'white', 6, 3, 0),
+(398, 20, 'pawn', 'white', 6, 4, 0),
+(399, 20, 'pawn', 'white', 6, 5, 0),
+(400, 20, 'pawn', 'white', 6, 6, 0),
+(401, 20, 'pawn', 'white', 6, 7, 0),
+(402, 20, 'rook', 'black', 0, 0, 0),
+(403, 20, 'knight', 'black', 0, 1, 0),
+(404, 20, 'bishop', 'black', 0, 2, 0),
+(405, 20, 'queen', 'black', 0, 3, 0),
+(406, 20, 'king', 'black', 0, 4, 0),
+(407, 20, 'bishop', 'black', 0, 5, 0),
+(408, 20, 'knight', 'black', 0, 6, 0),
+(409, 20, 'rook', 'black', 0, 7, 0),
+(410, 20, 'pawn', 'black', 1, 0, 0),
+(411, 20, 'pawn', 'black', 1, 1, 0),
+(412, 20, 'pawn', 'black', 1, 2, 0),
+(413, 20, 'pawn', 'black', 1, 3, 0),
+(414, 20, 'pawn', 'black', 1, 4, 0),
+(415, 20, 'pawn', 'black', 1, 5, 0),
+(416, 20, 'pawn', 'black', 1, 6, 0),
+(417, 20, 'pawn', 'black', 1, 7, 0),
+(418, 20, 'gazelle', 'white', 4, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -145,7 +191,7 @@ ALTER TABLE `shop`
 --
 ALTER TABLE `shopinventory`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `shopID` (`shopID`), 
+  ADD KEY `shopID` (`shopID`),
   ADD KEY `itemID` (`itemID`);
 
 --
@@ -162,7 +208,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `boards`
 --
 ALTER TABLE `boards`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT für Tabelle `items`
@@ -174,7 +220,7 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT für Tabelle `pieces`
 --
 ALTER TABLE `pieces`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=386;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=419;
 
 --
 -- AUTO_INCREMENT für Tabelle `shop`
@@ -212,12 +258,6 @@ ALTER TABLE `boards`
 ALTER TABLE `pieces`
   ADD CONSTRAINT `boardID` FOREIGN KEY (`boardID`) REFERENCES `boards` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-
--- das hier wurde selbst eingefügt, da ich es in SQL nicht fand
-
-ALTER TABLE `shopinventory`
-  ADD CONSTRAINT `shopID` FOREIGN KEY (`shopID`) REFERENCES `shop` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `itemID` FOREIGN KEY (`itemID`) REFERENCES `items` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
