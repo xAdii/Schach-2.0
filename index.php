bbf2ef76eeac25821a6853de6d185d21ba0e7ddb
chaaaos3 <schmolling.luise@gmail.com>
2026-04-07 15:03:15 +0200

    del: sql in index

--- index.php
+++ index.php
@@ -18,15 +18,6 @@ session_start();
 // DB config laden
 require_once './db_config.php';
 
-// Falls SQL Skript vorhanden, ausführen
-if (file_exists('drop_tables.sql')) {
-    $sql = file_get_contents('drop_tables.sql');
-    $pdo->exec($sql);
-}
-if (file_exists('schach_20.sql')) {
-    $sql = file_get_contents('schach_20.sql');
-    $pdo->exec($sql);
-}
 
 $userModel = new UserModel($pdo);
 $userController = new UserController($userModel);
